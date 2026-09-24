0d536e66994533be0807c9dd7817c1e78be667c2
dominik766 <lkts.dominik@telekimezotur.hu>
2026-09-24 10:46:50 +0200

    1.feladat

--- 01_StatikusElemek/Program.cs
+++ 01_StatikusElemek/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using _01_StatikusElemek;
+Console.WriteLine($"100Eur to HUF: {ValutaValto.EurToHuf(100)}");
+Console.WriteLine($"250Usd to Huf: {ValutaValto.UsdToHuf(250)}");
+Console.WriteLine($"Total conversions: {ValutaValto.GetOsszesAtvaltas()}");
---
+++ 01_StatikusElemek/ValutaValto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_StatikusElemek
+{
+    public static class ValutaValto
+    {
+        // valtasok
+        public const double EUR_HUF = 395.5;
+        public const double USD_HUF = 360.0;
+        // valtozo ami figyeli a konverziok szamat
+        private static int osszesAtvaltas = 0;
+        public static double EurToHuf(double eur)
+        {
+            osszesAtvaltas++;
+            return eur * EUR_HUF;
+        }
+        public static double UsdToHuf(double usd)
+        {
+            osszesAtvaltas++;
+            return usd * USD_HUF;
+        }
+        public static int GetOsszesAtvaltas()
+        {
+            return osszesAtvaltas;
+        }
+    }
+}
