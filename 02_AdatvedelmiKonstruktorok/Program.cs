e5057332cc164cad55ee8c7709a81df4f576c37a
dominik766 <lkts.dominik@telekimezotur.hu>
2026-09-25 10:43:57 +0200

    passz2

--- 02_AdatvedelmiKonstruktorok/Program.cs
+++ 02_AdatvedelmiKonstruktorok/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using _02_AdatvedelmiKonstruktorok;
+Bankszamla Anna = new Bankszamla("100");
+Bankszamla Bela = new Bankszamla("200", 100000, 50000);
+Anna.Befizetes(10000);
+Bela.Kivetel(20000);
+Console.WriteLine($"Anna szamlaszama: {Anna.Szamlaszam}, egyenlege: {Anna.Egyenleg}, hitelkerete: {Anna.HiteKeret}");
+Console.WriteLine($"Bela szamlaszama: {Bela.Szamlaszam}, egyenlege: {Bela.Egyenleg}, hitelkerete: {Bela.HiteKeret}");
